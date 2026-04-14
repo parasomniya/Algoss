4eca30c7aeab851bb555f686ec9948425b0fe6c8
parasomniya <parasomniya.m@gmail.com>
2026-04-14 19:20:32 +0700

    1

--- 1.c
+++ 1.c
@@ -1,106 +1,99 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define MAXN 400
-#define MAXM 79800
+typedef struct Node{
+    int v;
+    struct Node* next;
+}Node;
 
-static int head[MAXN + 1];
-static int to[MAXM + 1];
-static int next_edge[MAXM + 1];
-static int indegree[MAXN + 1];
-static int heap[MAXN + 1];
-static int order[MAXN + 1];
+Node* createNode(int v, Node* next){
+    Node* New = (Node*)malloc(sizeof(Node));
+    New->v = v;
+    New->next = next;
+    return New;
+}
 
-static void heap_push(int *size, int value) {
-    int pos = ++(*size);
-    heap[pos] = value;
+void insert(int* heap, int* size, int value){
+    (*size)++;
+    heap[*size] = value;
 
-    while (pos > 1 && heap[pos] < heap[pos / 2]) {
-        int temp = heap[pos];
-        heap[pos] = heap[pos / 2];
-        heap[pos / 2] = temp;
-        pos /= 2;
+    int i = *size;
+    while(i > 1 && heap[i] < heap[i / 2]){
+        int t = heap[i];
+        heap[i] = heap[i / 2];
+        heap[i / 2] = t;
+        i /= 2;
     }
 }
 
-static int heap_pop(int *size) {
-    int result = heap[1];
-    int value = heap[(*size)--];
-    int pos = 1;
+int getMin(int* heap, int* size){
+    int res = heap[1];
+    int x = heap[*size];
+    (*size)--;
 
-    while (pos * 2 <= *size) {
-        int child = pos * 2;
+    int i = 1;
+    while(i * 2 <= *size){
+        int j = i * 2;
+        if(j + 1 <= *size && heap[j + 1] < heap[j])
+            j++;
 
-        if (child + 1 <= *size && heap[child + 1] < heap[child]) {
-            child++;
-        }
-
-        if (heap[child] >= value) {
+        if(heap[j] >= x)
             break;
-        }
 
-        heap[pos] = heap[child];
-        pos = child;
+        heap[i] = heap[j];
+        i = j;
     }
 
-    heap[pos] = value;
-    return result;
+    heap[i] = x;
+    return res;
 }
 
-int main(void) {
+int main(){
     int n, m;
-    int edge_count = 0;
-    int heap_size = 0;
-    int produced = 0;
+    scanf("%d%d", &n, &m);
 
-    if (scanf("%d%d", &n, &m) != 2) {
-        return 0;
+    Node** a = (Node**)calloc(n + 1, sizeof(Node*));
+    int* in = (int*)calloc(n + 1, sizeof(int));
+    int* ans = (int*)malloc((n + 1) * sizeof(int));
+    int* heap = (int*)malloc((n + 1) * sizeof(int));
+
+    int size = 0;
+    int cnt = 0;
+
+    for(int i = 0; i < m; i++){
+        int b, c;
+        scanf("%d%d", &b, &c);
+        a[b] = createNode(c, a[b]);
+        in[c]++;
     }
 
-    for (int i = 1; i <= n; i++) {
-        head[i] = -1;
+    for(int i = 1; i <= n; i++){
+        if(in[i] == 0)
+            insert(heap, &size, i);
     }
 
-    for (int i = 0; i < m; i++) {
-        int b, a;
-        scanf("%d%d", &b, &a);
+    while(size > 0){
+        int x = getMin(heap, &size);
+        ans[cnt++] = x;
 
-        edge_count++;
-        to[edge_count] = a;
-        next_edge[edge_count] = head[b];
-        head[b] = edge_count;
-        indegree[a]++;
-    }
-
-    for (int i = 1; i <= n; i++) {
-        if (indegree[i] == 0) {
-            heap_push(&heap_size, i);
+        Node* p = a[x];
+        while(p != NULL){
+            in[p->v]--;
+            if(in[p->v] == 0)
+                insert(heap, &size, p->v);
+            p = p->next;
         }
     }
 
-    while (heap_size > 0) {
-        int v = heap_pop(&heap_size);
-        order[produced++] = v;
-
-        for (int edge = head[v]; edge != -1; edge = next_edge[edge]) {
-            int u = to[edge];
-            indegree[u]--;
-
-            if (indegree[u] == 0) {
-                heap_push(&heap_size, u);
-            }
-        }
-    }
-
-    if (produced != n) {
+    if(cnt != n){
         printf("bad course");
         return 0;
     }
 
-    for (int i = 0; i < n; i++) {
-        if (i > 0) {
+    for(int i = 0; i < cnt; i++){
+        if(i > 0)
             printf(" ");
-        }
-        printf("%d", order[i]);
+        printf("%d", ans[i]);
     }
 
     return 0;
