df77a2ab8978591a5b43b021e595a4bdd7d0dca1
parasomniya <parasomniya.m@gmail.com>
2026-04-28 14:40:07 +0700

    base

--- 1.c
+++ 1.c
@@ -1,47 +1,75 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define table_size 200005
 
-int a[55][55];
-int used[55];
+//список смежности
+typedef struct Node{
+    int v;
+    struct Node* next;
+}Node;
 
-void dfs(int v, int n){
-    used[v] = 1;
+Node* a[200005]; //массив списков смежностей
+//a[i] — список всех вершин, в которые можно попасть из i
+int dist[200005]; // расстояние от 1, если -1 то недостижима
+int q[200005]; // очередь
+int tail, head;
 
-    for (int i = 1; i <= n; i++){
-        if (a[v][i] == 1 && used[i] == 0)
-            dfs(i, n);
+//функция создания узла
+Node* createNode(int v, Node* next){
+    Node* New = (Node*)malloc(sizeof(Node));
+    New->v = v;
+    New->next = next;
+    return New;
+}
+
+//BFS
+void BFS(int start){
+    dist[start] = 0;
+    q[tail++] = start; // это в main потом перенести
+
+    while(head < tail){
+        int v = q[head++];
+
+        Node* p = a[v];
+        while (p != NULL){
+            int u = p->v;
+
+            if(dist[u] == -1){
+                dist[u] = dist[v] + 1;
+                q[tail++] = u;
+            }
+
+            p = p->next;
+        }
     }
 }
 
 int main(){
-    int n, m;
-    scanf("%d%d", &n, &m);
+    int N, M;
+    int start = 1;
 
-    for (int i = 0; i < m; i++){
-        int x, y;
-        scanf("%d%d", &x, &y);
+    head = 0;
+    tail = 0;
 
-        a[x][y] = 1;
-        a[y][x] = 1;
+    scanf("%d %d", &N, &M);
+    
+    for(int i = 1; i <= N; i++)
+        dist[i] = -1;
+    
+
+    for (int i = 0; i < M; i++){
+        int u, v;
+        scanf("%d %d", &u, &v);
+
+        a[u] = createNode(v, a[u]);
+        //u->v
+        //добавление вершины v в a[u]
     }
 
-    int s;
-    scanf("%d", &s);
+    BFS(start);
 
-    int shamans[15];
-
-    for (int i = 0; i < s; i++)
-        scanf("%d", &shamans[i]);
-
-    dfs(shamans[0], n);
-
-    for (int i = 0; i < s; i++){
-        if (used[shamans[i]] == 0){
-            printf("NO");
-            return 0;
-        }
-    }
-
-    printf("YES");
-
+    for (int i = 1; i <= N; i++)
+        printf("%d\n", dist[i]);
+    
     return 0;
 }
--- 2.c
+++ 2.c
@@ -1,37 +1,90 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define table_size 105
+#define q_size 10005
 
-int a[105][105];
-int used[105];
+char a[table_size][table_size]; //массив символов
+int dist[table_size][table_size]; //массив расстояний
 
-void dfs(int v, int n){
-    used[v] = 1;
+int qx[q_size];
+int qy[q_size];
 
-    for (int i = 1; i <= n; i++){
-        if (a[v][i] == 1 && used[i] == 0)
-            dfs(i, n);
+int head, tail;
+
+typedef struct coord{
+    int x;
+    int y;
+}coord;
+
+void BFS(coord start, int m, int n){
+    int dx[4] = {-1, 1, 0, 0};
+    int dy[4] = {0, 0, -1, 1};
+
+    dist[start.x][start.y] = 0;
+    qx[tail] = start.x;
+    qy[tail] = start.y;
+    tail++;
+
+    while(head < tail){
+        coord v;
+        v.x = qx[head];
+        v.y = qy[head];
+        head++;
+
+        for (int i = 0; i < 4; i++){
+            int nx = v.x + dx[i];
+            int ny = v.y + dy[i];
+
+            if (nx < 0 || nx >= m || ny < 0 || ny >= n)
+                continue;
+
+            if (a[nx][ny] == 'X')
+                continue;
+
+            if (dist[nx][ny] == -1){
+                dist[nx][ny] = dist[v.x][v.y] + 1;
+                qx[tail] = nx;
+                qy[tail] = ny;
+                tail++;
+            }
+        }
     }
 }
 
 int main(){
-    int n;
-    scanf("%d", &n);
+    int m, n;
+    coord start, finish;
+    start.x = start.y = 0;
+    finish.x = finish.y = 0;
 
-    int x, y;
-    while (scanf("%d%d", &x, &y) == 2){
-        a[x][y] = 1;
-        a[y][x] = 1;
+    scanf("%d%d", &m, &n);
+
+    for (int i = 0; i < m; i++){
+        scanf("%s", a[i]);
     }
 
-    int cnt = 0;
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            dist[i][j] = -1;
 
-    for (int i = 1; i <= n; i++){
-        if (used[i] == 0){
-            cnt++;
-            dfs(i, n);
+            if (a[i][j] == 'S'){
+                start.x = i;
+                start.y = j;
+            }
+
+            if (a[i][j] == 'F'){
+                finish.x = i;
+                finish.y = j;
+            }
         }
     }
 
-    printf("%d", cnt + 1);
+    head = 0;
+    tail = 0;
+
+    BFS(start, m, n);
+
+    printf("%d", dist[finish.x][finish.y]);
 
     return 0;
 }
